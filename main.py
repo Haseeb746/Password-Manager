767ec945fc6a2737b543bf0da91fcd0c955108ac
Haseeb Syed <syedhase@sheridancollege.ca>
2023-04-04 01:18:39 -0400

    Created all of the implementation and classes

--- main.py
+++ main.py
@@ -4,13 +4,40 @@ class PasswordManager:
     def __init__(self):
         self.key=None
         self.password=None
-        self.password={}
+        self.password_dic={}
 
-def create_key(self, path):
-    self.key = Fernet.generate_key
-    with open (path, 'wb') as f:
-        f.write(self.key)
+    def create_key(self, path):
+        self.key = Fernet.generate_key
+        with open (path, 'wb') as f:
+            f.write(self.key)
 
-def load_key(self, path):
-    with open (path, 'sb') as f:
-        self.key = f.read()
+    def load_key(self, path):
+        with open (path, 'sb') as f:
+            self.key = f.read()
+
+    def create_password_file(self, path, inital_values=None):
+        self.password_file= path
+
+        if inital_values is not None:
+            for key, value in inital_values.items():
+                self.add_password(key, value)
+
+    def load_password_file(self, path):
+        self.password_file=path
+        with open(path, 'r')as f:
+            for line in f:
+                site, encrypted = line.split(":")
+                self.password_dic[site] = Fernet(self.key).decrypt(encrypted.encode())
+    
+    def add_password(self, site, password):
+        self.password_dic[site] = password
+
+        if self.password_file is not None:
+            with open(self.password_file, '+a') as f:
+                encrypted = Fernet(self.key).encrypt(password.encode())
+                f.write(site + ":" +encrypted.decode() + "\n")
+
+    def get_password(self, site):
+        return self.password_dict[site]
+
+            
